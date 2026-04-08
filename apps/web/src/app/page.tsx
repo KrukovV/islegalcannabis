b55d7518932f328296206d38393894da8661a272
KrukovV <krukovv@gmail.com>
2026-04-08 23:00:04 +0300

    feat: mount map on root (reuse new-map runtime)

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,1 @@
-import styles from "./page.module.css";
-import MapSection from "./_components/MapSection";
-import { getBuildStamp } from "@/lib/buildStamp";
-export default function Home() {
-  const buildStamp = getBuildStamp();
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <MapSection buildStamp={buildStamp} />
-      </main>
-    </div>
-  );
-}
+export { default } from "./new-map/page";
